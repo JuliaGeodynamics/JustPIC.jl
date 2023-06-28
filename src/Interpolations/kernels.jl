fc2c650ea8bfd9e2e440fd46a1aaf6953b6acc9e
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-06-28 10:52:30 +0200

    replace fma by muladd and splat tuples

--- src/Interpolations/kernels.jl
+++ src/Interpolations/kernels.jl
@@ -5,7 +5,7 @@
 
 Linear interpolation between `f(x0)=v0` and `f(x1)=v1` at the normalized coordinate t
 """
-@inline lerp(t, v0, v1) = fma(t, v1, fma(-t, v0, v0))
+@inline lerp(t, v0, v1) = muladd(t, v1, muladd(-t, v0, v0))
 
 """
     bilinear(tx, ty, v00, v10, v01, v11)
@@ -17,7 +17,7 @@ Linear interpolation between `f(x0,y0)=v0` and `f(x1,y1)=v1` at the normalized c
 end
 
 @inline function ndlinear(t::NTuple{2,T1}, v::NTuple{4,T2}) where {T1,T2}
-    return ndlinear(t[1], t[2], v[1], v[2], v[3], v[4])
+    return ndlinear(t..., v...)
 end
 
 bilinear(tx, ty, v00, v10, v01, v11) = ndlinear(tx, ty, v00, v10, v01, v11)
@@ -36,7 +36,7 @@ Linear interpolation between `f(x0,y0,z0)=v0` and `f(x1,y1,z1)=v1` at the normal
 end
 
 @inline function ndlinear(t::NTuple{3,T1}, v::NTuple{8,T2}) where {T1,T2}
-    return ndlinear(t[1], t[2], t[3], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8])
+    return ndlinear(t..., v...)
 end
 
 @inline function trilinear(tx, ty, tz, v000, v100, v001, v101, v010, v110, v011, v111)
