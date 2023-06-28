7cbbf49c77b17db6c935084f7b7813846b9f0985
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-06-28 11:07:18 +0200

    fix examples

--- scripts/single_particle_advection.jl
+++ scripts/single_particle_advection.jl
@@ -1,14 +1,14 @@
-ENV["PS_PACKAGE"] = "Threads"
+ENV["PS_PACKAGE"] = "CUDA"
 
 using JustPIC
 using CellArrays
 using ParallelStencil
 using GLMakie
-@init_parallel_stencil(Threads, Float64, 2)
+@init_parallel_stencil(CUDA, Float64, 2)
 
 const TA = ENV["PS_PACKAGE"] == "CUDA" ? JustPIC.CUDA.CuArray : Array
 
-function init_particles(nxcell, max_xcell, min_xcell, x, y, dx, dy, nx, ny)
+function init_particle(nxcell, max_xcell, min_xcell, x, y, dx, dy, nx, ny)
     ni = nx, ny
     ncells = nx * ny
     np = max_xcell * ncells
@@ -16,27 +16,20 @@ function init_particles(nxcell, max_xcell, min_xcell, x, y, dx, dy, nx, ny)
 
     inject = @fill(false, nx, ny, eltype=Bool)
     index = @fill(false, ni..., celldims=(max_xcell,), eltype=Bool) 
-    
-    @parallel_indices (i, j) function fill_coords_index(px, py, index)    
-        # lower-left corner of the cell
-        x0, y0 = x[i], y[j]
-        # fill index array
-        for l in 1:nxcell
-            @cell px[l, i, j] = x0 + dx * rand(0.05:1e-5:0.95)
-            @cell py[l, i, j] = y0 + dy * rand(0.05:1e-5:0.95)
-            @cell index[l, i, j] = true
-        end
-        return nothing
-    end
 
-    @parallel (1:nx, 1:ny) fill_coords_index(px, py, index)    
+    i  ,j  = Int(nx÷3), Int(ny÷3)
+    x0, y0 = x[i], y[j]
+
+    @cell    px[1, i, j] = x0 + dx * rand(0.05:1e-5: 0.95)
+    @cell    py[1, i, j] = y0 + dy * rand(0.05:1e-5: 0.95)
+    @cell index[1, i, j] = true
 
     return Particles(
         (px, py), index, inject, nxcell, max_xcell, min_xcell, np, (nx, ny)
     )
 end
 
-cell_array(false, (max_xcell,), nx, ny)
+@inline init_particle_fields_cellarrays(particles, ::Val{N}) where N = ntuple(_ -> @fill(0.0, size(particles.coords[1])..., celldims=(cellsize(particles.index))), Val(N))
 
 function expand_range(x::AbstractRange)
     dx = x[2] - x[1]
@@ -48,8 +41,8 @@ function expand_range(x::AbstractRange)
 end
 
 # Analytical flow solution
-vx_stream(x, y) =  250 * sin(π*x) * cos(π*y)
-vy_stream(x, y) = -250 * cos(π*x) * sin(π*y)
+vx_stream(x, y) =   250 * sin(π*x) * cos(π*y)
+vy_stream(x, y) =  -250 * cos(π*x) * sin(π*y)
 g(x) = Point2f(
     vx_stream(x[1], x[2]),
     vy_stream(x[1], x[2])
@@ -58,14 +51,14 @@ g(x) = Point2f(
 function main()
     # Initialize particles -------------------------------
     nxcell, max_xcell, min_xcell = 24, 48, 18
-    n = 65
+    n = 257
     nx = ny = n-1
     Lx = Ly = 1.0
     # nodal vertices
     xvi = xv, yv = range(0, Lx, length=n), range(0, Ly, length=n)
     dxi = dx, dy = xv[2] - xv[1], yv[2] - yv[1]
     # nodal centers
-    xc, yc = range(0+dx/2, Lx-dx/2, length=n-1), range(0+dy/2, Ly-dy/2, length=n-1)
+    xci = xc, yc = range(0+dx/2, Lx-dx/2, length=n-1), range(0+dy/2, Ly-dy/2, length=n-1)
     # staggered grid velocity nodal locations
     grid_vx = xv, expand_range(yc)
     grid_vy = expand_range(xc), yv
@@ -74,26 +67,40 @@ function main()
         nxcell, max_xcell, min_xcell, xvi..., dxi..., nx, ny
     )
 
+    # allocate particle field
+    particle_args = ()
+
     # Cell fields -------------------------------
-    Vx = TA([vx_stream(x, y) for x in grid_vx[1], y in grid_vx[2]])
-    Vy = TA([vy_stream(x, y) for x in grid_vy[1], y in grid_vy[2]])
-    T  = TA([y for x in xv, y in yv])
-    V  = Vx, Vy
+    Vx  = TA([vx_stream(x, y) for x in grid_vx[1], y in grid_vx[2]])
+    Vy  = TA([vy_stream(x, y) for x in grid_vy[1], y in grid_vy[2]])
+    V   = Vx, Vy
 
     dt = min(dx / maximum(abs.(Vx)),  dy / maximum(abs.(Vy)))
 
+    pxv = particles.coords[1].data;
+    pyv = particles.coords[2].data;
+    idxv = particles.index.data;
+    p = [(pxv[idxv][1], pyv[idxv][1])]
+
     # Advection test
-    particle_args = pT, = init_cell_arrays(particles, Val(1))
-    grid2particle!(pT, xvi, T, particles.coords)
-    
-    niter = 150
-    for _ in 1:niter
+    niter = 750
+    for iter in 1:niter
         advection_RK!(particles, V, grid_vx, grid_vy, dt, 2 / 3)
         shuffle_particles!(particles, xvi, particle_args)
-        particle2grid!(T, pT, xvi, particles.coords)
+
+        pxv = particles.coords[1].data;
+        pyv = particles.coords[2].data;
+        idxv = particles.index.data;
+        p_i = (pxv[idxv][1], pyv[idxv][1])
+        push!(p, p_i)
     end
 
-    f = heatmap(xvi..., T)
+    f, ax, = streamplot(g, xvi...)
+    lines!(ax, p, color=:red)
+    f
+
 end
 
-@time main()
+@time f = main()
+save("single_particle_advection.png", f)
+f
--- scripts/temperature_advection.jl
+++ scripts/temperature_advection.jl
@@ -1,11 +1,14 @@
 ENV["PS_PACKAGE"] = "Threads"
 
-using Particles
+using JustPIC
 using CellArrays
 using ParallelStencil
+using GLMakie
 @init_parallel_stencil(Threads, Float64, 2)
 
-function init_particles_cellarrays(nxcell, max_xcell, min_xcell, x, y, dx, dy, nx, ny)
+const TA = ENV["PS_PACKAGE"] == "CUDA" ? JustPIC.CUDA.CuArray : Array
+
+function init_particles(nxcell, max_xcell, min_xcell, x, y, dx, dy, nx, ny)
     ni = nx, ny
     ncells = nx * ny
     np = max_xcell * ncells
@@ -19,8 +22,8 @@ function init_particles_cellarrays(nxcell, max_xcell, min_xcell, x, y, dx, dy, n
         x0, y0 = x[i], y[j]
         # fill index array
         for l in 1:nxcell
-            @cell px[l, i, j] = x0 + dx * rand(0.05:1e-5: 0.95)
-            @cell py[l, i, j] = y0 + dy * rand(0.05:1e-5: 0.95)
+            @cell px[l, i, j] = x0 + dx * rand(0.05:1e-5:0.95)
+            @cell py[l, i, j] = y0 + dy * rand(0.05:1e-5:0.95)
             @cell index[l, i, j] = true
         end
         return nothing
@@ -28,33 +31,12 @@ function init_particles_cellarrays(nxcell, max_xcell, min_xcell, x, y, dx, dy, n
 
     @parallel (1:nx, 1:ny) fill_coords_index(px, py, index)    
 
-    return Particle(
+    return Particles(
         (px, py), index, inject, nxcell, max_xcell, min_xcell, np, (nx, ny)
     )
 end
 
-function init_particles_single(nxcell, max_xcell, min_xcell, x, y, dx, dy, nx, ny)
-    ni = nx, ny
-    ncells = nx * ny
-    np = max_xcell * ncells
-    px, py = ntuple(_ -> @fill(NaN, ni..., celldims=(max_xcell,)) , Val(2))
-
-    inject = @fill(false, nx, ny, eltype=Bool)
-    index = @fill(false, ni..., celldims=(max_xcell,), eltype=Bool) 
-
-    i  ,j  = Int(nx÷3), Int(ny÷3)
-    x0, y0 = x[i], y[j]
-
-    @cell    px[1, i, j] = x0 + dx * rand(0.05:1e-5: 0.95)
-    @cell    py[1, i, j] = y0 + dy * rand(0.05:1e-5: 0.95)
-    @cell index[1, i, j] = true
-
-    return Particle(
-        (px, py), index, inject, nxcell, max_xcell, min_xcell, np, (nx, ny)
-    )
-end
-
-@inline init_particle_fields_cellarrays(particles, ::Val{N}) where N = ntuple(_ -> @fill(0.0, size(particles.coords[1])..., celldims=(cellsize(particles.index))), Val(N))
+cell_array(false, (max_xcell,), nx, ny)
 
 function expand_range(x::AbstractRange)
     dx = x[2] - x[1]
@@ -66,102 +48,52 @@ function expand_range(x::AbstractRange)
 end
 
 # Analytical flow solution
-stream(x, y)    = 250/π * sin(π*x) * sin(π*y)
-vx_stream(x, y) =   250 * sin(π*x) * cos(π*y)
-vy_stream(x, y) =  -250 * cos(π*x) * sin(π*y)
+vx_stream(x, y) =  250 * sin(π*x) * cos(π*y)
+vy_stream(x, y) = -250 * cos(π*x) * sin(π*y)
 g(x) = Point2f(
     vx_stream(x[1], x[2]),
     vy_stream(x[1], x[2])
 )
 
-# Initialize particles -------------------------------
-nxcell, max_xcell, min_xcell = 24, 48, 18
-n = 51
-nx = ny = n-1
-Lx = Ly = 1.0
-# nodal vertices
-xvi = xv, yv = range(0, Lx, length=n), range(0, Ly, length=n)
-dxi = dx, dy = xv[2] - xv[1], yv[2] - yv[1]
-# nodal centers
-xci = xc, yc = range(0+dx/2, Lx-dx/2, length=n-1), range(0+dy/2, Ly-dy/2, length=n-1)
-# staggered grid velocity nodal locations
-grid_vx = xv, expand_range(yc)
-grid_vy = expand_range(xc), yv
+function main()
+    # Initialize particles -------------------------------
+    nxcell, max_xcell, min_xcell = 24, 48, 18
+    n = 65
+    nx = ny = n-1
+    Lx = Ly = 1.0
+    # nodal vertices
+    xvi = xv, yv = range(0, Lx, length=n), range(0, Ly, length=n)
+    dxi = dx, dy = xv[2] - xv[1], yv[2] - yv[1]
+    # nodal centers
+    xc, yc = range(0+dx/2, Lx-dx/2, length=n-1), range(0+dy/2, Ly-dy/2, length=n-1)
+    # staggered grid velocity nodal locations
+    grid_vx = xv, expand_range(yc)
+    grid_vy = expand_range(xc), yv
 
-# particles = init_particles_cellarrays(
-#     nxcell, max_xcell, min_xcell, xvi..., dxi..., nx, ny
-# )
+    particles = init_particles(
+        nxcell, max_xcell, min_xcell, xvi..., dxi..., nx, ny
+    )
 
-particles = init_particles_single(
-    nxcell, max_xcell, min_xcell, xvi..., dxi..., nx, ny
-)
+    # Cell fields -------------------------------
+    Vx = TA([vx_stream(x, y) for x in grid_vx[1], y in grid_vx[2]])
+    Vy = TA([vy_stream(x, y) for x in grid_vy[1], y in grid_vy[2]])
+    T  = TA([y for x in xv, y in yv])
+    V  = Vx, Vy
 
-# allocate particle field
-pT, = init_particle_fields_cellarrays(particles, Val(1))
-particle_args = (pT,)
+    dt = min(dx / maximum(abs.(Vx)),  dy / maximum(abs.(Vy)))
 
-# Cell fields -------------------------------
-Vx  = [vx_stream(x, y) for x in grid_vx[1], y in grid_vx[2]]
-Vy  = [vy_stream(x, y) for x in grid_vy[1], y in grid_vy[2]]
-V   = Vx, Vy
-T   = [y for x in xv, y in yv]
-Xv  = [x for x in xv, y in yv]
-Yv  = [y for x in xv, y in yv]
-Xc  = [x for x in xc, y in yc]
-Yc  = [y for x in xc, y in yc]
+    # Advection test
+    particle_args = pT, = init_cell_arrays(particles, Val(1))
+    grid2particle!(pT, xvi, T, particles.coords)
+    
+    niter = 150
+    for _ in 1:niter
+        advection_RK!(particles, V, grid_vx, grid_vy, dt, 2 / 3)
+        shuffle_particles!(particles, xvi, particle_args)
+        particle2grid!(T, pT, xvi, particles.coords)
+    end
 
-dt = min(dx / maximum(abs.(Vx)),  dy / maximum(abs.(Vy))) * 1
-
-p = Tuple{Float64, Float64}[]
-
-pxv = particles.coords[1].data;
-pyv = particles.coords[2].data;
-idxv = particles.index.data;
-p = [(pxv[idxv][1], pyv[idxv][1])]
-t = [0.0]
-
-# Advection test
-niter = 150
-for iter in 1:niter
-    advection_RK!(particles, V, grid_vx, grid_vy, dt, 2 / 3)
-    shuffle_particles!(particles, xvi, particle_args)
-
-    pxv = particles.coords[1].data;
-    pyv = particles.coords[2].data;
-    idxv = particles.index.data;
-    p_i = (pxv[idxv][1], pyv[idxv][1])
-    push!(p, p_i)
+    f = heatmap(xvi..., T)
 end
 
-# f,ax, = scatter(Xv[:], Yv[:], color=:black, markersize=5)
-f,ax, = streamplot(g, xvi...)
-lines!(ax, p, color=:red)
-f
-
-# # grid2particle!(pT, xvi, T, particles.coords)
-
-# # advect particles in space
-# advection_RK!(particles, V, grid_vx, grid_vy, dt, 2 / 3)
-# # advect particles in memory
-# shuffle_particles!(particles, xvi, particle_args)
-# clean_particles!(particles, xvi, particle_args)
-# # check if we need to inject particles
-# inject = check_injection(particles)
-# inject && inject_particles_phase!(particles, pPhases, (pT, ), (T, ), xvi)
-# # update phase ratios
-# @parallel (@idx ni) phase_ratios_center(phase_ratios.center, pPhases)
-# # interpolate fields from particle to grid vertices
-# particle2grid!(T, pT, xvi, particles.coords)
-
-
-
-p = particles.coords
-ppx, ppy = p;
-pxv = ppx.data[:];
-pyv = ppy.data[:];
-ppT = pT.data[:];
-idxv = particles.index.data[:];
-err = (abs.(ppT.-pyv))
-
-heatmap(xvi..., T)
-f,ax,h = scatter(pxv, pyv, color=err)
+@time main()
