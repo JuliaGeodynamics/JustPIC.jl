2b9cf6450c623efe7c93ee813ed73b2117afa3bb
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-07-25 16:53:08 +0200

    refactor to use ParallelStencil.jl

--- src/Interpolations/particle_to_grid.jl
+++ src/Interpolations/particle_to_grid.jl
@@ -1,19 +1,26 @@
-@inline function distance_weight(a::NTuple{N,T}, b::NTuple{N,T}; order=2) where {N,T}
-    return inv(distance(a, b)^order)
+## LAUNCHERS
+
+function particle2grid!(
+    F::Array, Fp::AbstractArray, xi::NTuple{2,T}, particle_coords
+) where {T}
+    di = grid_size(xi)
+
+    @parallel (@idx size(F)) particle2grid!(F, Fp, xi, particle_coords, di)
+
+    return
 end
 
-@inline @generated function bilinear_weight(
-    a::NTuple{N,T}, b::NTuple{N,T}, di::NTuple{N,T}
-) where {N,T}
-    quote
-        val = one(T)
-        Base.Cartesian.@nexprs $N i ->
-            @inbounds val *= one(T) - abs(a[i] - b[i]) * inv(di[i])
-        return val
-    end
+@parallel_indices (inode, jnode) function particle2grid!(F, Fp, xi, particle_coords, di)
+    _particle2grid!(F, Fp, inode, jnode, xi, particle_coords, di)
+    return
 end
 
-## CPU 2D
+@parallel_indices (inode, jnode, knode) function particle2grid!(F, Fp, knode, xi, particle_coords, di)
+    _particle2grid!(F, Fp, inode, jnode, knode, xi, particle_coords, di)
+    return
+end
+
+## INTERPOLATION KERNEL 2D
 
 @inbounds function _particle2grid!(
     F, Fp, inode, jnode, xi::NTuple{2,T}, p, di
@@ -47,75 +54,7 @@ end
     return F[inode, jnode] = ωxF / ω
 end
 
-function particle2grid!(
-    F::Array, Fp::AbstractArray, xi::NTuple{2,T}, particle_coords
-) where {T}
-    di = grid_size(xi)
-    Threads.@threads for jnode in axes(F, 2)
-        for inode in axes(F, 1)
-            _particle2grid!(F, Fp, inode, jnode, xi, particle_coords, di)
-        end
-    end
-end
-
-## CUDA 2D
-
-function _particle2grid!!(F, Fp, xi, p, di::NTuple{2,T}) where {T}
-    inode = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    jnode = (blockIdx().y - 1) * blockDim().y + threadIdx().y
-
-    @inbounds if (inode ≤ length(xi[1])) && (jnode ≤ length(xi[2]))
-        px, py = p # particle coordinates
-        nx, ny = size(F)
-        xvertex = (xi[1][inode], xi[2][jnode]) # cell lower-left coordinates
-        ω, ωxF = 0.0, 0.0 # init weights
-
-        # iterate over cells around i-th node
-        for joffset in -1:0
-            jvertex = joffset + jnode
-            for ioffset in -1:0
-                ivertex = ioffset + inode
-                # make sure we stay within the grid
-                if (1 ≤ ivertex < nx) && (1 ≤ jvertex < ny)
-                    # iterate over cell
-                    @inbounds for i in cellaxes(px)
-                        p_i = @cell(px[i, ivertex, jvertex]), @cell(py[i, ivertex, jvertex])
-                        # ignore lines below for empty memory
-                        any(isnan, p_i) && continue
-                        ω_i = bilinear_weight(xvertex, p_i, di)
-                        ω += ω_i
-                        ωxF += ω_i * @cell(Fp[i, ivertex, jvertex])
-                    end
-                end
-            end
-        end
-
-        F[inode, jnode] = ωxF / ω
-    end
-    return nothing
-end
-
-function particle2grid!(
-    F::CuArray, Fp::AbstractArray, xi::NTuple{2,T}, particle_coords
-) where {T}
-    di       = grid_size(xi)
-    nx, ny   = size(particle_coords[1])
-    n        = 16
-    nblocksx = ceil(Int, nx / n)
-    nblocksy = ceil(Int, ny / n)
-    threadsx = n
-    threadsy = n
-
-    CUDA.@sync begin
-        @cuda threads = (threadsx, threadsy) blocks = (nblocksx, nblocksy) _particle2grid!!(
-            F, Fp, xi, particle_coords, di
-        )
-    end
-
-    return nothing
-end
-
-## CPU 3D
+## INTERPOLATION KERNEL 3D
 
 @inbounds function _particle2grid!(
     F, Fp, inode, jnode, knode, xi::NTuple{3,T}, p, di
@@ -154,81 +93,19 @@ end
     return F[inode, jnode, knode] = ωF * inv(ω)
 end
 
-function particle2grid!(
-    F::AbstractArray, Fp::AbstractArray, xi::NTuple{3,T}, particle_coords
-) where {T}
-    di = grid_size(xi)
-    Threads.@threads for knode in axes(F, 3)
-        for jnode in axes(F, 2), inode in axes(F, 1)
-            _particle2grid!(F, Fp, inode, jnode, knode, xi, particle_coords, di)
-        end
-    end
+## OTHERS
+
+@inline function distance_weight(a::NTuple{N,T}, b::NTuple{N,T}; order=2) where {N,T}
+    return inv(distance(a, b)^order)
 end
 
-## CUDA 3D
-
-function particle2grid!(
-    F::CuArray, Fp::AbstractArray, xi::NTuple{3,T}, particle_coords
-) where {T}
-    di = grid_size(xi)
-    nx, ny, nz = size(Fp)
-    threadsx = 8
-    threadsy = 8
-    threadsz = 4
-    nblocksx = ceil(Int, nx / threadsx)
-    nblocksy = ceil(Int, ny / threadsy)
-    nblocksz = ceil(Int, nz / threadsz)
-
-
-    CUDA.@sync begin
-        @cuda threads = (threadsx, threadsy, threadsz) blocks = (
-            nblocksx, nblocksy, nblocksz
-        ) _particle2grid!!(F, Fp, xi, particle_coords, di)
+@inline @generated function bilinear_weight(
+    a::NTuple{N,T}, b::NTuple{N,T}, di::NTuple{N,T}
+) where {N,T}
+    quote
+        val = one(T)
+        Base.Cartesian.@nexprs $N i ->
+            @inbounds val *= one(T) - abs(a[i] - b[i]) * inv(di[i])
+        return val
     end
-    
-    return nothing
-end
-
-function _particle2grid!!(F, Fp, xi, p, di::NTuple{3,T}) where {T}
-    inode = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    jnode = (blockIdx().y - 1) * blockDim().y + threadIdx().y
-    knode = (blockIdx().z - 1) * blockDim().z + threadIdx().z
-    lx, ly, lz = length.(xi)
-
-    @inbounds if (inode ≤ lx) && (jnode ≤ ly) && (knode ≤ lz)
-        px, py, pz = p # particle coordinates
-        nx, ny, nz = size(F)
-        xvertex    = (xi[1][inode], xi[2][jnode], xi[3][knode]) # cell lower-left coordinates
-        ω, ωF      = 0.0, 0.0 # init weights
-
-        # iterate over cells around i-th node
-        for koffset in -1:0
-            kvertex = koffset + knode
-            for joffset in -1:0
-                jvertex = joffset + jnode
-                for ioffset in -1:0
-                    ivertex = ioffset + inode
-                    # make sure we stay within the grid
-                    if (1 ≤ ivertex < nx) && (1 ≤ jvertex < ny) && (1 ≤ kvertex < nz)
-                        # iterate over cell
-                        @inbounds for i in cellaxes(Fp)
-                            p_i = (
-                                @cell(px[i, ivertex, jvertex, kvertex]),
-                                @cell(py[i, ivertex, jvertex, kvertex]),
-                                @cell(pz[i, ivertex, jvertex, kvertex]),
-                            )
-                            # ignore lines below for unused allocations
-                            isnan(p_i[1]) && continue
-                            ω_i = bilinear_weight(xvertex, p_i, di)
-                            ω += ω_i
-                            ωF += ω_i * @cell Fp[i, ivertex, jvertex, kvertex]
-                        end
-                    end
-                end
-            end
-        end
-
-        F[inode, jnode, knode] = ωF / ω
-    end
-    return nothing
 end
