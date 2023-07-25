cdadbb0bd83171c9d22e03788545c01ecc2ed159
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-07-25 17:12:50 +0200

    add FLIP

--- src/Interpolations/grid_to_particle.jl
+++ src/Interpolations/grid_to_particle.jl
@@ -1,22 +1,24 @@
+## CLASSIC PIC ------------------------------------------------------------------------------------------------
+
 # LAUNCHERS
 
-function grid2particle!(Fp::AbstractArray, xvi, F, particle_coords)
+function grid2particle!(Fp::AbstractArray, xvi, F::AbstractArray, particle_coords)
     di = grid_size(xvi)
     ni = length.(xvi)
 
-    @parallel (@idx ni.-1) grid2particle!(Fp, F, xvi, di, particle_coords)
+    @parallel (@idx ni.-1) grid2particle_classic!(Fp, F, xvi, di, particle_coords)
 
 end
 
-@parallel_indices (inode, jnode) function grid2particle!(Fp, F, xvi, di::NTuple{2, Any}, particle_coords)
-    inner_grid2particle!(
+@parallel_indices (inode, jnode) function grid2particle_classic!(Fp, F, xvi, di::NTuple{2, Any}, particle_coords)
+    _grid2particle_classic!(
         Fp, particle_coords, xvi, di, F, (inode, jnode)
     )
     return nothing
 end
 
-@parallel_indices (inode, jnode, knode) function grid2particle!(Fp, F, xvi, di::NTuple{3, Any}, particle_coords)
-    inner_grid2particle!(
+@parallel_indices (inode, jnode, knode) function grid2particle_classic!(Fp, F, xvi, di::NTuple{3, Any}, particle_coords)
+    _grid2particle_classic!(
         Fp, particle_coords, xvi, di, F, (inode, jnode, knode)
     )
     return nothing
@@ -24,7 +26,7 @@ end
 
 # INNERMOST INTERPOLATION KERNEL
 
-@inline function inner_grid2particle!(Fp, p, xvi, di::NTuple{N, T}, F, idx) where {N, T}
+@inline function _grid2particle_classic!(Fp, p, xvi, di::NTuple{N, T}, F, idx) where {N, T}
     # iterate over all the particles within the cells of index `idx` 
     for ip in cellaxes(Fp)
         # cache particle coordinates 
@@ -51,4 +53,51 @@ function _grid2particle(
     Fp = ndlinear(ti, Fi)
     
     return Fp
+end
+
+## FULL PARTICLE PIC ------------------------------------------------------------------------------------------
+
+# LAUNCHERS
+
+function grid2particle!(Fp::AbstractArray, xvi, F::AbstractArray, F0::AbstractArray, particle_coords; α = 0.0)
+    di = grid_size(xvi)
+    ni = length.(xvi)
+
+    @parallel (@idx ni.-1) grid2particle_full!(Fp, F, F0, xvi, di, particle_coords, α)
+
+end
+
+@parallel_indices (inode, jnode) function grid2particle_full!(Fp, F, F0, xvi, di::NTuple{2, Any}, particle_coords, α)
+    _grid2particle_full!(
+        Fp, particle_coords, xvi, di, F, F0, (inode, jnode), α
+    )
+    return nothing
+end
+
+@parallel_indices (inode, jnode, knode) function grid2particle_full!(Fp, F, F0, xvi, di::NTuple{3, Any}, particle_coords, α)
+    _grid2particle_full!(
+        Fp, particle_coords, xvi, di, F, F0, (inode, jnode, knode), α
+    )
+    return nothing
+end
+
+# INNERMOST INTERPOLATION KERNEL
+
+@inline function _grid2particle_full!(Fp, p, xvi, di::NTuple{N, T}, F, F0, idx, α) where {N, T}
+    # iterate over all the particles within the cells of index `idx` 
+    for ip in cellaxes(Fp)
+        # cache particle coordinates 
+        pᵢ = ntuple(i -> (@cell p[i][ip, idx...]), Val(N))
+
+        # skip lines below if there is no particle in this pice of memory
+        any(isnan, pᵢ) && continue 
+
+        Fᵢ     = @cell Fp[ip, idx...]
+        F_pic  = _grid2particle(pᵢ, xvi, di, F, idx)
+        ΔF     = F_pic - _grid2particle(pᵢ, xvi, di, F0, idx)
+        F_flip = Fᵢ + ΔF
+        # Interpolate field F onto particle
+        @cell Fp[ip, idx...] = F_pic * α + F_flip*(1.0 - α)
+
+    end
 end
