d2fb9a58329c61f136e49f188b18809c9c634585
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-12-05 23:22:05 +0100

    classic particles layout & advection

---
+++ src/Classic/advection.jl
@@ -0,0 +1,113 @@
+## 2D SPECIFIC FUNCTIONS 
+"""
+    advection_RK!(particles, V, grid_vx, grid_vy, dt, α)
+
+Advect `particles` with the velocity field `V::NTuple{dims, AbstractArray{T,dims}`
+on the staggered grid given by `grid_vx` and `grid_vy`using a Runge-Kutta2 scheme 
+with `α` and time step `dt`.
+
+    xᵢ ← xᵢ + h*( (1-1/(2α))*f(t,xᵢ) + f(t, y+α*h*f(t,xᵢ))) / (2α)
+        α = 0.5 ==> midpoint
+        α = 1   ==> Heun
+        α = 2/3 ==> Ralston
+"""
+# Main Runge-Kutta advection function for 2D staggered grids
+
+function advection_RK!(
+    particles::ClassicParticles, V, grid_vx::NTuple{2,T}, grid_vy::NTuple{2,T}, dt, α
+) where {T}
+    # unpack 
+    (; coords, grid_step) = particles
+    px, py = coords
+    np = nparticles(particles)
+
+    # Need to transpose grid_vy and Vy to reuse interpolation kernels
+    grid_vi = grid_vx, grid_vy
+    local_limits = inner_limits(grid_vi)
+
+    # launch parallel advection kernel
+    @parallel (1:np) _advection_RK!(
+        coords, V, grid_vi, local_limits, grid_step, dt, α
+    )
+
+    return nothing
+end
+
+# ParallelStencil fuction Runge-Kutta advection for 2D staggered grids
+@parallel_indices (ipart) function _advection_RK!(
+    p, V::NTuple{N,T}, grid, local_limits, dxi, dt, α
+) where {T}
+    pᵢ = p[ipart]
+    p_new = advect_classic_particle_RK(
+        pᵢ, V, grid, local_limits, dxi, dt, α
+    )
+    p[ipart] = SVector{N, T}(p_new)
+
+    return nothing
+end
+
+# DIMENSION AGNOSTIC KERNELS
+
+function advect_classic_particle_RK(
+    p0::NTuple{N,T},
+    V::NTuple{N,AbstractArray{T,N}},
+    grid_vi,
+    local_limits,
+    dxi,
+    dt,
+    α,
+) where {T,N}
+
+    _α = inv(α)
+    ValN = Val(N)
+
+    # interpolate velocity to current location
+    idx = get_cell(p0, dxi)
+    vp0 = ntuple(ValN) do i
+        Base.@_inline_meta
+        local_lims = local_limits[i]
+
+        # if this condition is met, it means that the particle
+        # went outside the local rank domain. It will be removed 
+        # during shuffling
+        v = if check_local_limits(local_lims, p0) 
+            interp_velocity_grid2particle(p0, grid_vi[i], dxi, V[i], idx)
+        else
+            zero(T)
+        end
+    end
+
+    # advect α*dt
+    p1 = ntuple(ValN) do i
+        Base.@_inline_meta
+        muladd(vp0[i], dt * α, p0[i])
+    end
+
+    # interpolate velocity to new location
+    idx = get_cell(p1, dxi)
+    vp1 = ntuple(ValN) do i
+        Base.@_inline_meta
+        local_lims = local_limits[i]
+        # if this condition is met, it means that the particle
+        # went outside the local rank domain. It will be removed 
+        # during shuffling
+        v = if check_local_limits(local_lims, p1)
+            interp_velocity_grid2particle(p1, grid_vi[i], dxi, V[i], idx)
+        else
+            zero(T)
+        end
+    end
+
+    # final advection step
+    pf = ntuple(ValN) do i
+        Base.@_propagate_inbounds_meta
+        Base.@_inline_meta
+        if α == 0.5
+            @muladd p0[i] + dt * vp1[i]
+        else
+            @muladd p0[i] + dt * ((1.0 - 0.5 * _α) * vp0[i] + 0.5 * _α * vp1[i])
+        end
+    end
+
+    return pf
+end
---
+++ src/Classic/particles.jl
@@ -0,0 +1,34 @@
+using StaticArrays
+
+abstract type AsbtractParticles end
+
+struct ClassicParticles{N, NP, T} <: AsbtractParticles
+    coords::T
+    grid_step::NTuple{N, Float64}
+    ncells::NTuple{N, Int64}
+
+    function ClassicParticles(nparticles, grid_lims, grid_step::NTuple{N, Float64}) where {N}
+        ncells = Int64.(grid_lims ./ grid_step)
+        coords = [@SVector(rand(N)) .* grid_lims for _ in 1:nparticles*prod(ncells)]
+        nparticles = length(coords)
+        new{N, nparticles, typeof(coords)}(coords, grid_step, ncells)
+    end
+end
+
+# Cell indexing functions
+
+@inline get_cell_i(x, p::ClassicParticles) = Int64(x ÷ p.grid_step[1] + 1) 
+@inline get_cell_j(y, p::ClassicParticles) = Int64(y ÷ p.grid_step[2] + 1) 
+@inline get_cell_k(z, p::ClassicParticles) = Int64(z ÷ p.grid_step[3] + 1) 
+
+function get_cell(xi, p::ClassicParticles{N}) where N 
+    ntuple(Val(N)) do i 
+        Base.@_inline_meta
+        Int64(xi[i] ÷ p.grid_step[i] + 1) 
+    end
+end
+
+# Others
+
+@inline nparticles(::ClassicParticles{N, NP}) where {N, NP} = NP
+@inline dimension(::ClassicParticles{N}) where {N} = N
