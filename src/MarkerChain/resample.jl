a35147a6170fe80faafb491ac6e781220a7fb944
albert-de-montserrat <albert.de.montserrat@gmail.com>
2024-02-02 16:54:08 +0100

    rework resample!

--- src/MarkerChain/resample.jl
+++ src/MarkerChain/resample.jl
@@ -1,64 +1,77 @@
 function resample!(p::MarkerChain)
+
+    # resampling launch kernel
     @parallel_indices (i) function resample!(
         coords, cell_vertices, index, min_xcell, max_xcell, dx_cells
     )
-
-        # cell particles coordinates
-        x_cell, y_cell = coords[1][i], coords[2][i]
-        # number of particles in the cell
-        np = count(index_cell)
-        # dx of the new chain
-        dx_chain = dx_cells / (np + 1)
-        # resample the cell if the number of particles is  
-        # less than min_xcell or it is too distorted
-        do_resampling = (np < min_xcell) * isdistorded(x_cell, dx_chain)
-
-        if do_resampling
-            # lower-left corner of the cell
-            x0 = cell_vertices[i]
-            # fill index array
-            for ip in 1:nxcell
-                # x query point
-                @cell px[ip, i] = xq = x0 + dx_chain * ip
-                # interpolated y coordinated
-                yq = if 1 < i < length(x_cell)
-                    # inner cells; this is true (ncells-2) consecutive times
-                    interp1D_inner(xq, coords, i)
-                else
-                    # first and last cells
-                    interp1D_extremas(xq, x_cell, y_cell)
-                end
-                @cell py[ip, i] = yq
-                @cell index[ip, i] = true
-            end
-            # fill empty memory locations
-            for ip in (nxcell + 1):max_xcell
-                @cell px[ip, i] = NaN
-                @cell py[ip, i] = NaN
-                @cell index[ip, i] = false
-            end
-        end
+        resample_cell!(coords, cell_vertices, index, min_xcell, max_xcell, dx_cells, i)
         return nothing
     end
 
-    (; coords, cell_vertices, min_xcell, max_xcell) = p
-    nx = length(x) - 1
-    dx_cells = cell_length(chain)
-
+    (; coords, index, cell_vertices, min_xcell, max_xcell) = p
+    nx = length(cell_vertices) - 1
+    dx_cells = cell_length(chain) 
+    
     # call kernel
     @parallel (1:nx) resample!(coords, cell_vertices, index, min_xcell, max_xcell, dx_cells)
     return nothing
 end
 
+function resample_cell!(
+    coords::NTuple{2, T}, cell_vertices, index, min_xcell, max_xcell, dx_cells, I
+) where T
+
+    # cell particles coordinates
+    x_cell, y_cell = coords[1][I], coords[2][I]
+    px, py = coords[1], coords[2]
+
+    cell_vertex = cell_vertices[I]
+    # number of particles in the cell
+    np = count(index[I])
+    # dx of the new chain
+    dx_chain = dx_cells / (np + 1)
+    # resample the cell if the number of particles is  
+    # less than min_xcell or it is too distorted
+    do_resampling = (np < min_xcell) * isdistorded(x_cell, dx_chain)
+
+    if do_resampling
+        # lower-left corner of the cell
+        x0 = cell_vertex
+        # fill index array
+        for ip in 1:min_xcell
+            # x query point
+            @cell px[ip, I] = xq = x0 + dx_chain * ip
+            # interpolated y coordinated
+            yq = if 1 < I < length(x_cell) 
+                # inner cells; this is true (ncells-2) consecutive times
+                interp1D_inner(xq, coords, I)
+            else 
+                # first and last cells
+                interp1D_extremas(xq, x_cell, y_cell)
+            end
+            @cell py[ip, I] = yq
+            @cell index[ip, I] = true
+        end
+        # fill empty memory locations
+        for ip in (min_xcell+1):max_xcell
+            @cell px[ip, I] = NaN
+            @cell py[ip, I] = NaN
+            @cell index[ip, I] = false
+        end
+    end
+    return nothing
+end
+
+
 function isdistorded(x_cell, dx_ideal)
-    for ip in eachindex(x_cell)[1:(end - 1)]
+    for ip in eachindex(x_cell)[1:end-1]
         # current particle
         current_x = x_cell[ip]
         # if there is no particle in this memory location,
         # we do nothing
         isnan(current_x) && continue
         # next particle
-        next_x = x_cell[ip + 1]
+        next_x = x_cell[ip+1]
         # check wether next memory location holds a particle;
         # if thats the case, find the next particle
         if isnan(next_x)
