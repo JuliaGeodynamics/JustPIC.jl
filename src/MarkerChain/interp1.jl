3ea4109da197c7d5cf6f2584e51e670afcbfb121
albert-de-montserrat <albert.de.montserrat@gmail.com>
2024-02-06 12:10:24 +0100

    fix interp1 kernels

--- src/MarkerChain/interp1.jl
+++ src/MarkerChain/interp1.jl
@@ -8,22 +8,23 @@ function interp1D_extremas(xq, x, y)
         # interpolation
         if x0 < xq < x1
             y0, y1 = y[j], y[j + 1]
-            _interp1D(xq, x0, x1, y0, y1)
+            return _interp1D(xq, x0, x1, y0, y1)
         end
 
         # extrapolation
         if xq < x_lo
             x0, x1 = x[1], x[2]
             y0, y1 = y[1], y[2]
-            _interp1D(xq, x0, x1, y0, y1)
+            return _interp1D(xq, x0, x1, y0, y1)
         end
 
         if xq > x_hi
             x0, x1 = x[end], x[end - 1]
             y0, y1 = y[end], y[end - 1]
-            _interp1D(xq, x0, x1, y0, y1)
+            return _interp1D(xq, x0, x1, y0, y1)
         end
     end
+    error("xq outside domain")
 end
 
 function interp1D_inner(xq, x, y, cell_coords, I::Integer)
@@ -34,20 +35,36 @@ function interp1D_inner(xq, x, y, cell_coords, I::Integer)
         # interpolation
         if x0 < xq < x1
             y0, y1 = y[j], y[j + 1]
-            _interp1D(xq, x0, x1, y0, y1)
+            return _interp1D(xq, x0, x1, y0, y1)
         end
 
         # interpolate using the last particle of left-neighbouring cell
         if xq < x_lo
             x0, y0 = left_cell_right_particle(cell_coords, I)
             x1, y1 = x[1], y[1]
-            _interp1D(xq, x0, x1, y0, y1)
+            return _interp1D(xq, x0, x1, y0, y1)
         end
         # interpolate using the first particle of right-neighbouring cell
         if xq > x_hi
             x0, y0 = x[end], y[end]
             x1, y1 = right_cell_left_particle(cell_coords, I)
-            _interp1D(xq, x0, x1, y0, y1)
+            return _interp1D(xq, x0, x1, y0, y1)
         end
     end
+    error("xq outside domain")
+end
+
+
+@inline right_cell_left_particle(cell_coords, I::Int) = @cell(cell_coords[1][1, I+1]), @cell(cell_coords[2][1, I+1])
+
+@inline function left_cell_right_particle(cell_coords, I)
+    px = cell_coords[1][I-1]
+    ip = findlast(!isnan, px)
+    return px[ip], @cell(cell_coords[2][ip, I-1])
+end
+
+@inline function is_above_surface(xq, yq, coords, cell_vertices)
+    I = cell_index(xq, cell_vertices)
+    x_cell, y_cell = coords[1][I], coords[2][I]
+    return yq > interp1D_inner(xq, x_cell, y_cell, coords, I)
 end
