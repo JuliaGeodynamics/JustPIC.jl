099fd557c15759a100c5e2df4796ff83a997bdd1
albert-de-montserrat <albert.de.montserrat@gmail.com>
2024-02-06 16:46:40 +0100

    remove dx as an argument in init_markerchain

--- src/MarkerChain/init.jl
+++ src/MarkerChain/init.jl
@@ -1,4 +1,4 @@
-function init_markerchain(backend, nxcell, min_xcell, max_xcell, x, initial_elevation, dx)
+function init_markerchain(backend, nxcell, min_xcell, max_xcell, xv, initial_elevation)
     
     @parallel_indices (i) function fill_coords_index!(
         px, py, index, x, initial_elevation, dx_chain, nxcell, max_xcell
@@ -15,13 +15,14 @@ function init_markerchain(backend, nxcell, min_xcell, max_xcell, x, initial_elev
     end
 
     nx = length(x) - 1
+    dx = xv[2]-xv[1]
     dx_chain = dx / (nxcell + 1)
     px, py = ntuple(_ -> @fill(NaN, (nx,), celldims = (max_xcell,)), Val(2))
     index = @fill(false, (nx,), celldims = (max_xcell,), eltype = Bool)
 
     @parallel (1:nx) fill_coords_index!(
-        px, py, index, x, initial_elevation, dx_chain, nxcell, max_xcell
+        px, py, index, xv, initial_elevation, dx_chain, nxcell, max_xcell
     )
 
-    return MarkerChain(backend, (px, py), index, x, min_xcell, max_xcell)
+    return MarkerChain(backend, (px, py), index, xv, min_xcell, max_xcell)
 end
