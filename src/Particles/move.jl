467f73d01e6514a4340373134a0f20724486c41e
albert-de-montserrat <albert.de.montserrat@gmail.com>
2024-02-19 11:46:49 +0100

    update move_kernel!

--- src/Particles/move.jl
+++ src/Particles/move.jl
@@ -23,13 +23,13 @@ function _move_particles!(coords, grid, dxi, index, domain_limits, idx, args)
     # coordinate of the lower-most-left coordinate of the parent cell 
     corner_xi = corner_coordinate(grid, idx)
     # iterate over neighbouring (child) cells
-    move_kernel!(coords, corner_xi, dxi, index, domain_limits, args, idx)
+    move_kernel!(coords, corner_xi, grid, dxi, index, domain_limits, args, idx)
 
     return nothing
 end
 
 function move_kernel!(
-    coords, corner_xi, dxi, index, domain_limits, args::NTuple{N2,T}, idx::NTuple{N1,Int64}
+    coords, corner_xi, grid, dxi, index, domain_limits, args::NTuple{N2,T}, idx::NTuple{N1,Int64}
 ) where {N1,N2,T}
 
     # iterate over particles in child cell 
@@ -51,7 +51,7 @@ function move_kernel!(
         domain_check && continue
 
         # new cell indices
-        new_cell = get_cell(pᵢ, dxi)
+        new_cell = get_index(pᵢ, grid)
         # hold particle variables
         current_args = @inbounds cache_args(args, ip, idx)
         # remove particle from child cell
