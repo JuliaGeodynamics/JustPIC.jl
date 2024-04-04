6374daf322a820f7994c6a63be82f93599a57e3e
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-04-04 23:51:22 +0200

    reorganize

--- src/Advection/RK2.jl
+++ src/Advection/RK2.jl
@@ -1,30 +1,3 @@
-
-@inline function first_stage(
-    integrator::RungeKutta2, dt, particle_velocity::NTuple{N,T}, particle_coordinate::NTuple{N,T}
-) where {N,T}
-    (; α) = integrator
-    return @. @muladd particle_coordinate + dt * α * particle_velocity
-end
-
-@inline function second_stage(
-    integrator::RungeKutta2,
-    dt,
-    particle_velocity0::NTuple{N,T},
-    particle_velocity1::NTuple{N,T},
-    particle_coordinate::NTuple{N,T},
-) where {N,T}
-    (; α) = integrator
-    p = if α == 0.5
-        @. @muladd particle_coordinate + dt * particle_velocity1
-    else
-        @. @muladd particle_coordinate +
-        dt * (
-            (1.0 - 0.5 * inv(α)) * particle_velocity0 + 0.5 * inv(α) * particle_velocity1
-        )
-    end
-    return p
-end
-
 @inline function advect_particle(
     method::RungeKutta2,
     p0::NTuple{N,T},
@@ -51,3 +24,28 @@ end
     return p2
 end
 
+@inline function first_stage(
+    integrator::RungeKutta2, dt, particle_velocity::NTuple{N,T}, particle_coordinate::NTuple{N,T}
+) where {N,T}
+    (; α) = integrator
+    return @. @muladd particle_coordinate + dt * α * particle_velocity
+end
+
+@inline function second_stage(
+    integrator::RungeKutta2,
+    dt,
+    particle_velocity0::NTuple{N,T},
+    particle_velocity1::NTuple{N,T},
+    particle_coordinate::NTuple{N,T},
+) where {N,T}
+    (; α) = integrator
+    p = if α == 0.5
+        @. @muladd particle_coordinate + dt * particle_velocity1
+    else
+        @. @muladd particle_coordinate +
+        dt * (
+            (1.0 - 0.5 * inv(α)) * particle_velocity0 + 0.5 * inv(α) * particle_velocity1
+        )
+    end
+    return p
+end
