40358a21de91c09cff8fe4895540f1bd19ffcb65
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-04-06 12:07:26 +0200

    some reorganization

--- src/Advection/Euler.jl
+++ src/Advection/Euler.jl
@@ -3,23 +3,3 @@
 ) where {N,T}
     return @. @muladd particle_coordinate + dt * particle_velocity
 end
-
-function advect_particle(
-    method::Euler,
-    p0::NTuple{N,T},
-    V::NTuple{N,AbstractArray{T,N}},
-    grid_vi,
-    local_limits,
-    dxi,
-    dt,
-    idx::NTuple,
-) where {T,N}
-
-    # interpolate velocity to current location
-    vp0 = interp_velocity2particle(p0, grid_vi, local_limits, dxi, V, idx)
-
-    # first advection stage x = x + v * dt * α
-    p1 = first_stage(method, dt, vp0, p0)
-
-    return p1
-end
--- src/Advection/RK2.jl
+++ src/Advection/RK2.jl
@@ -1,29 +1,3 @@
-@inline function advect_particle(
-    method::RungeKutta2,
-    p0::NTuple{N,T},
-    V::NTuple{N,AbstractArray{T,N}},
-    grid_vi,
-    local_limits,
-    dxi,
-    dt,
-    idx::NTuple,
-) where {T,N}
-
-    # interpolate velocity to current location
-    vp0 = interp_velocity2particle(p0, grid_vi, local_limits, dxi, V, idx)
-
-    # first advection stage x = x + v * dt * α
-    p1 = first_stage(method, dt, vp0, p0)
-
-    # interpolate velocity to new location
-    vp1 = interp_velocity2particle(p1, grid_vi, local_limits, dxi, V, idx)
-
-    # final advection step
-    p2 = second_stage(method, dt, vp0, vp1, p0)
-
-    return p2
-end
-
 @inline function first_stage(
     integrator::RungeKutta2, dt, particle_velocity::NTuple{N,T}, particle_coordinate::NTuple{N,T}
 ) where {N,T}
