3087b0097cf38a8fbedeb2f3c1548cfa44d048b3
albert-de-montserrat <albert.de.montserrat@gmail.com>
2024-04-22 15:59:07 +0200

    add lerp docstring

--- src/Interpolations/ndlerp.jl
+++ src/Interpolations/ndlerp.jl
@@ -1,7 +1,26 @@
+"""
+    lerp(t, v0, v1)
+
+Linearly interpolates between `v0` and `v1` using the parameter `t`.
+
+# Arguments
+- `t`: The interpolation parameter.
+- `v0`: The starting value.
+- `v1`: The ending value.
+
+"""
 @inline lerp(t, v0, v1) = fma(t, v1, fma(-t, v0, v0))
 
-@inline lerp(v, t::NTuple{nD,T}) where {nD,T} = lerp(v, t, 0, Val(nD - 1))
-@inline lerp(v, t::NTuple{nD,T}, i, ::Val{N}) where {nD,N,T} =
-    lerp(t[N + 1], lerp(v, t, i, Val(N - 1)), lerp(v, t, i + 2^N, Val(N - 1)))
-@inline lerp(v, t::NTuple{nD,T}, i, ::Val{0}) where {nD,T} =
-    lerp(t[1], v[i + 1], v[i + 2])
+"""
+    lerp(v, t::NTuple{nD,T}) where {nD,T}
+
+Linearly interpolates the value `v` between the elements of the tuple `t`.
+This function is specialized for tuples of length `nD`.
+
+# Arguments
+- `v`: The value to be interpolated.
+- `t`: The tuple of values to interpolate between.
+"""
+@inline lerp(v, t::NTuple{nD,T})              where {nD,T}   = lerp(v, t, 0, Val(nD - 1))
+@inline lerp(v, t::NTuple{nD,T}, i, ::Val{N}) where {nD,N,T} = lerp(t[N + 1], lerp(v, t, i, Val(N - 1)), lerp(v, t, i + 2^N, Val(N - 1)))
+@inline lerp(v, t::NTuple{nD,T}, i, ::Val{0}) where {nD,T}   = lerp(t[1], v[i + 1], v[i + 2])
