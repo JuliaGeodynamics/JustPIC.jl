4d8cfd4f7f4cbaa4a26e6450bb29f47137306c63
tduretz <duretz@uni-frankfurt.de>
2024-09-25 15:40:14 +0200

    add restart example

--- scripts/SolCx/pureshear_ALE_restart.jl
+++ scripts/SolCx/pureshear_ALE_restart.jl
@@ -1,4 +1,4 @@
-using Statistics, LinearAlgebra, Printf, Base.Threads, CairoMakie
+using Statistics, LinearAlgebra, Printf, Base.Threads, CairoMakie, JLD2
 const year     = 365*3600*24
 const USE_GPU  = false
 
@@ -6,6 +6,8 @@ using JustPIC, JustPIC._2D
 const backend = JustPIC.CPUBackend 
 
 const ALE    = true
+last_step    = 100
+restart      = false
 
 using ParallelStencil
 using ParallelStencil.FiniteDifferences2D
@@ -31,37 +33,64 @@ end
 end
 
 @parallel_indices (I...) function SetVelocity(V, verts, ε̇bg)
-
     if I[1]<=size(V.x,1) &&  I[2]<=size(V.x,2)
         V.x[I...] =  verts.x[I[1]]*ε̇bg
     end
-
     if I[1]<=size(V.y,1) &&  I[2]<=size(V.y,2)
         V.y[I...] = -verts.y[I[2]]*ε̇bg
     end
-
 return nothing
 end
-
 
 
 function main()
 
     @printf("Running on %d thread(s)\n", nthreads())
-    
+
+    # Load Breakpoint data
+    if restart
+        file  = @sprintf("./Breakpoint%05d.jld2", last_step)
+        data  = load(file)
+        particles     = data["particles"]
+        phases        = data["phases"]   
+        phase_ratios  = data["phase_ratios"]
+        particle_args = data["particle_args"]
+        xlims = data["xlims"]
+        ylims = data["ylims"]
+        t     = data["t"]
+        Nt    = last_step + 100
+        it0   = last_step + 1
+        ε̇bg   = -1.
+    else
+        xlims = [-0.5 , 0.5]
+        ylims = [-0.5 , 0.5]
+        t     = 0.
+        Nt    = 100
+        it0   = 1
+        ε̇bg   = 1.
+    end
+
     # Parameters
-    L    = (x=1., y=1.)
+    L    = (x =(xlims[2]-xlims[1]), y =(ylims[2]-ylims[1]) )
     Nc   = (x=41, y=41 )
     Nv   = (x=Nc.x+1,   y=Nc.y+1   )
     Δ    = (x=L.x/Nc.x, y=L.y/Nc.y )
-    Nt   = 100
     Nout = 10
     C    = 0.25
 
     # Model extent
-    verts     = (x=LinRange(-L.x/2, L.x/2, Nv.x), y=LinRange(-L.y/2, L.y/2, Nv.y))
-    cents     = (x=LinRange(-Δ.x/2+L.x/2, L.x/2-Δ.x/2, Nc.x), y=LinRange(-Δ.y/2+L.y/2, L.y+Δ.y/2-L.y/2, Nc.y))
-    cents_ext = (x=LinRange(-Δ.x/2-L.x/2, L.x/2+Δ.x/2, Nc.x+2), y=LinRange(-Δ.y/2-L.y/2, L.y+Δ.y/2+L.y/2, Nc.y+2))
+    cents_ext = (
+        x      = LinRange(xlims[1]-Δ.x/2, xlims[2]+Δ.x/2, Nc.x+2),
+        y      = LinRange(ylims[1]-Δ.y/2, ylims[2]+Δ.y/2, Nc.y+2),
+    )
+    cents     = (
+        x      = LinRange(xlims[1]+Δ.x/2, xlims[2]-Δ.x/2, Nc.x+2),
+        y      = LinRange(ylims[1]+Δ.y/2, ylims[2]-Δ.y/2, Nc.y+2),
+    )
+    verts = (
+        x      = LinRange(xlims[1], xlims[2], Nc.x+1),
+        y      = LinRange(ylims[1], ylims[2], Nc.y+1),
+    )
     xlims     = [verts.x[1] , verts.x[end]]
     ylims     = [verts.y[1] , verts.y[end]]
 
@@ -74,29 +103,31 @@ function main()
     )
 
     # Set velocity field
-    ε̇bg = 1.
     @parallel SetVelocity(V, verts, ε̇bg)
  
-    # Initialize particles -------------------------------
-    nxcell, max_xcell, min_xcell = 12, 24, 5
-    particles = init_particles(
-        backend, 
-        nxcell, 
-        max_xcell,
-        min_xcell, 
-        values(verts),
-        values(Δ),
-        values(Nc)
-    ) # random position by default
+    if !restart 
+        # Initialize particles -------------------------------
+        nxcell, max_xcell, min_xcell = 12, 24, 5
+        particles = init_particles(
+            backend, 
+            nxcell, 
+            max_xcell,
+            min_xcell, 
+            values(verts),
+            values(Δ),
+            values(Nc)
+        ) # random position by default
 
-    # Initialise phase field
-    particle_args = phases, = init_cell_arrays(particles, Val(1))  # cool
+        # Initialise phase field
+        particle_args = phases, = init_cell_arrays(particles, Val(1))  # cool
 
-    @parallel InitialFieldsParticles!(phases, particles.coords..., particles.index)
+        @parallel InitialFieldsParticles!(phases, particles.coords..., particles.index)
+    end
 
     phase_ratios = JustPIC._2D.PhaseRatios(backend, 2, values(Nc));
     phase_ratios_vertex!(phase_ratios, particles, values(verts), phases) 
     phase_ratios_center!(phase_ratios, particles, values(verts), phases) 
+    Npart = sum(particles.index.data)  
 
     println(" 
     it => 0
@@ -105,7 +136,6 @@ function main()
     ")
 
     # Time step
-    t  = 0.
     Δt = C * min(Δ...) / max(maximum(abs.(V.x)), maximum(abs.(V.y)))
     @show Δt
 
@@ -116,7 +146,9 @@ function main()
     Vyc     = 0.5*(V.y[2:end-1,1:end-1] .+ V.y[2:end-1,2:end-0])
     Vmag    = sqrt.(Vxc.^2 .+ Vyc.^2)
 
-    for it=1:Nt
+    for it=it0:Nt
+
+        @printf("Step %05d, #particles = %06d\n", it, Npart)
 
         t += Δt
 
@@ -126,14 +158,15 @@ function main()
         move_particles!(particles, values(verts), particle_args);
         inject_particles_phase!(particles, phases, (), (), values(verts))
         phase_ratios_vertex!(phase_ratios, particles, values(verts), phases);
-        phase_ratios_center!(phase_ratios, particles, values(cents), phases);    
+        phase_ratios_center!(phase_ratios, particles, values(cents), phases);  
+        Npart = sum(particles.index.data)  
   
         if ALE
             xlims[1] += xlims[1]*ε̇bg*Δt 
             xlims[2] += xlims[2]*ε̇bg*Δt
             ylims[1] -= ylims[1]*ε̇bg*Δt 
             ylims[2] -= ylims[2]*ε̇bg*Δt
-            @show L  = ( x =(xlims[2]-xlims[1]), y =(ylims[2]-ylims[1]) )  
+            L  = ( x =(xlims[2]-xlims[1]), y =(ylims[2]-ylims[1]) )  
             Δ  = (x=L.x/Nc.x, y=L.y/Nc.y )
             cents_ext = (
                 x      = LinRange(xlims[1]-Δ.x/2, xlims[2]+Δ.x/2, Nc.x+2),
@@ -152,9 +185,8 @@ function main()
         end
         
         if mod(it, Nout) == 0 || it==1
-            @show Npart = sum(particles.index.data)
+            @show
             particle_density = [sum(p) for p in particles.index]
-            @show size(particle_density)
             # visualisation
             p    = particles.coords
             ppx, ppy = p
@@ -169,6 +201,12 @@ function main()
             ylims!(ax, verts.y[1], verts.y[end])
             display(f)
         end       
+
+        # Save breakpoint
+        if it==100
+            @show file = @sprintf("./Breakpoint%05d.jld2", Nt)
+            jldsave(file; particles, phases, phase_ratios, particle_args, xlims, ylims, t)
+        end
     end
 
     return nothing
