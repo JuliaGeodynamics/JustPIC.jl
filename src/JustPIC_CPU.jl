7648178d2e0af2a8402d18171aacd47443931379
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-10-24 18:09:46 +0200

    initialize PS inside __init__()

--- src/JustPIC_CPU.jl
+++ src/JustPIC_CPU.jl
@@ -9,7 +9,10 @@ using ..JustPIC
 
 import ..JustPIC: AbstractBackend, CPUBackend
 
-@init_parallel_stencil(Threads, Float64, 2)
+function __init__()
+    @init_parallel_stencil(Threads, Float64, 2)
+    return nothing
+end
 
 export CA
 
@@ -37,7 +40,10 @@ using ..JustPIC
 
 import ..JustPIC: AbstractBackend, CPUBackend
 
-@init_parallel_stencil(Threads, Float64, 3)
+function __init__()
+    @init_parallel_stencil(Threads, Float64, 3)
+    return nothing
+end
 
 export CA
 
