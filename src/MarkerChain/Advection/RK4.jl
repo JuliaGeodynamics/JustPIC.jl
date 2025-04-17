59200d5f5613ca466f1f43052b521d6d606516c4
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-04-17 15:29:02 +0200

    marker chain

--- src/MarkerChain/Advection/RK4.jl
+++ src/MarkerChain/Advection/RK4.jl
@@ -7,20 +7,12 @@
         dxi,
         dt,
     ) where {N, T}
-    # interpolate velocity to current location
-    v1 = interp_velocity2particle_markerchain(p0, grid_vi, local_limits, dxi, V)
-    k1 = @. dt * v1
 
-    # second stage
-    v2 = interp_velocity2particle_markerchain(p0 .+ k1 ./ 2, grid_vi, local_limits, dxi, V)
-    k2 = @. dt * v2 / 2
-
-    v3 = interp_velocity2particle_markerchain(p0 .+ k2 ./ 2, grid_vi, local_limits, dxi, V)
-    k3 = @. dt * v3 / 2
-
-    v4 = interp_velocity2particle_markerchain(p0 .+ k3, grid_vi, local_limits, dxi, V)
-    k4 = @. dt * v4
-
-    p = @. p0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+    k1 = interp_velocity2particle_markerchain(p0, grid_vi, local_limits, dxi, V)
+    k2 = interp_velocity2particle_markerchain(p0 .+ dt .* k1 ./ 2, grid_vi, local_limits, dxi, V)
+    k3 = interp_velocity2particle_markerchain(p0 .+ dt .* k2 ./ 2, grid_vi, local_limits, dxi, V)
+    k4 = interp_velocity2particle_markerchain(p0 .+ dt .* k3, grid_vi, local_limits, dxi, V)
+ 
+    p = @. p0 + dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6
     return p
 end
