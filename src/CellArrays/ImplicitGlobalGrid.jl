2a58271c1cc20768561a6b65fa9b2735a79ab491
Albert de Montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2025-04-25 23:48:09 +0200

    Improve halo update

--- src/CellArrays/ImplicitGlobalGrid.jl
+++ src/CellArrays/ImplicitGlobalGrid.jl
@@ -2,11 +2,11 @@ function update_cell_halo!(
         x::Vararg{CellArray{S, N, D, A}, NA}
     ) where {NA, S, N, D, A <: AbstractArray}
     ni = size(x[1])
-    tmp = @fill(0, ni..., eltype = eltype(x[1].data))
+    tmp = @fill(0e0, ni..., eltype = eltype(x[1].data))
 
     for xᵢ in x
         for ip in cellaxes(xᵢ)
-            copyto!(tmp, field(xᵢ, ip))
+            @parallel (@idx ni) copy_field!(tmp, xᵢ, ip)
             update_halo!(tmp)
             @parallel (@idx ni) copy_field!(xᵢ, tmp, ip)
         end
@@ -15,6 +15,11 @@ function update_cell_halo!(
 end
 
 @parallel_indices (I...) function copy_field!(A::CellArray, B::AbstractArray, ip)
-    @index A[ip, I...] = B[I...]
+    @inbounds @index A[ip, I...] = B[I...]
+    return nothing
+end
+
+@parallel_indices (I...) function copy_field!(B::AbstractArray, A::CellArray, ip)
+    @inbounds B[I...] = @index A[ip, I...]
     return nothing
 end
