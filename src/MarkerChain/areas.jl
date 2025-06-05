4034e66c81189dc8d191d4797e5132a97573930a
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-06-05 15:53:01 +0200

    use GridGeometryUtils to compute rock volume fractions

--- src/MarkerChain/areas.jl
+++ src/MarkerChain/areas.jl
@@ -7,349 +7,240 @@ function compute_rock_fraction!(ratios, chain::MarkerChain, xvi, dxi)
 end
 
 function compute_area_below_chain_centers!(ratio_center, chain, xvi, dxi)
-    topo_x, topo_y = chain.cell_vertices, chain.h_vertices
+    topo_y = chain.h_vertices
     nx, ny = size(ratio_center)
     @parallel (1:nx, 1:ny) _compute_area_below_chain_center!(
-        ratio_center, topo_x, topo_y, xvi..., dxi
+        ratio_center, topo_y, xvi..., dxi
     )
     return nothing
 end
 
 @parallel_indices (i, j) function _compute_area_below_chain_center!(
-        ratio, topo_x, topo_y, xv, yv, dxi
-    )
-    _, dy = dxi
+        ratio::AbstractArray{T}, topo_y, xv, yv, dxi
+    ) where {T}
 
-    # cell min max coordinates
-    x_min_cell, x_max_cell = xv[i], xv[i + 1]
-    y_min_cell, y_max_cell = yv[j], yv[j + 1]
-    topo_xᵢ = topo_x[i], topo_x[i + 1]
-    topo_yᵢ = topo_y[i], topo_y[i + 1]
+    # cell origin
+    ox = xv[i]
+    oy = yv[j]
 
-    isbelow = topo_yᵢ[1] - y_min_cell ≥ dy && topo_yᵢ[2] - y_min_cell ≥ dy
-    isabove = y_max_cell - topo_yᵢ[1] ≥ dy && y_max_cell - topo_yᵢ[2] ≥ dy
-
-    if isbelow
-        ratio[i, j] = one(eltype(topo_xᵢ))
-
-    elseif isabove
-        ratio[i, j] = zero(eltype(topo_xᵢ))
-
-    else
-        # compute area at cell center
-        area_rock = compute_area_below_chain(
-            topo_xᵢ, topo_yᵢ, x_min_cell, x_max_cell, y_min_cell, y_max_cell, dxi
-        )
-        area_cell = prod(dxi)
-        ratio[i, j] = clamp(area_rock / area_cell, 0, 1)
-    end
+    p1 = GridGeometryUtils.Point(ox, topo_y[i])
+    p2 = GridGeometryUtils.Point(xv[i+1], topo_y[i+1])
+    s  = Segment(p1, p2)
+    
+    r = Rectangle((ox,oy), dxi...)
+    ratio[i, j] = cell_rock_area(s, r)
 
     return nothing
 end
 
 function compute_area_below_chain_vx!(ratio_velocity, chain, xvi, dxi)
-    topo_x, topo_y = chain.cell_vertices, chain.h_vertices
+    topo_y = chain.h_vertices
     nx, ny = size(ratio_velocity)
-    masks_x = ((-0.5, 0.0), (0.0, 0.5))
-    masks_y = ((0.0, 1.0), (0.0, 1.0))
+    mask_x = (-0.5, 0e0) .* dxi[1]
+
     @parallel (1:nx, 1:ny) _compute_area_below_chain_vx!(
-        ratio_velocity, topo_x, topo_y, masks_x, masks_y, xvi..., nx, dxi
+        ratio_velocity, topo_y, mask_x, xvi..., nx, dxi
     )
     return nothing
 end
 
 @parallel_indices (i, j) function _compute_area_below_chain_vx!(
-        ratios, topo_x, topo_y, masks_x, masks_y, xv, yv, n, dxi
-    )
+        ratios::AbstractArray{T}, topo_y, mask_x, xv, yv, nx, dxi
+    ) where {T}
     dx, dy = dxi
-    area_cell = prod(dxi) / 2
-    ratios[i, j] = zero(eltype(dx))
-    correction = 1
-    for (c, ii) in enumerate((i - 1):i)
-        if 0 < ii < n
-            if 1 < i < n
-                correction = 0.5
-            else
-                correction = 2.0e0
-            end
-            x_min_cell, x_max_cell = xv[i] .+ masks_x[c] .* dx
-            y_min_cell, y_max_cell = yv[j] .+ masks_y[c] .* dy
-
-            topo_xᵢ = topo_x[ii], topo_x[ii + 1]
-            topo_yᵢ = topo_y[ii], topo_y[ii + 1]
-            p1 = topo_xᵢ[1], topo_yᵢ[1]
-            p2 = topo_xᵢ[2], topo_yᵢ[2]
-
-            # recompute topography intersections
-            if do_intersect(p1, p2, x_min_cell, x_max_cell, y_min_cell, y_max_cell)
-                p1, p2 = get_intersections(
-                    (p1, p2), x_min_cell, x_max_cell, y_min_cell, y_max_cell
-                )
-                topo_xᵢ = p1[1], p1[2]
-                topo_xᵢ = p2[1], p2[2]
-            end
-
-            isbelow = topo_yᵢ[1] - y_min_cell ≥ dy && topo_yᵢ[2] - y_min_cell ≥ dy
-            isabove = y_max_cell - topo_yᵢ[1] ≥ dy && y_max_cell - topo_yᵢ[2] ≥ dy
-
-            if isbelow
-                ratios[i, j] = min(1, ratios[i, j] + 1)
-
-            elseif isabove
-                ratios[i, j] = zero(eltype(topo_xᵢ))
-
-            else
-                # compute area at cell center
-                area_rock = compute_area_below_chain(
-                    topo_xᵢ, topo_yᵢ, x_min_cell, x_max_cell, y_min_cell, y_max_cell, dxi
-                )
-                ratios[i, j] += clamp(correction * area_rock / area_cell, 0, 1)
-            end
-        end
+    half_dx = dx / 2
+    half_dy = dy / 2
+    c = 0
+    ω = 0 # weight for the average
+    tmp = zero(T)
+    # we can cache the potential coordinates
+    x, y =  if 1 < i < nx
+        (xv[i-1], xv[i], xv[i+1]), (topo_y[i-1], topo_y[i], topo_y[i+1])
+    elseif i == 1
+        (xv[i], xv[i], xv[i+1]), (topo_y[i], topo_y[i], topo_y[i+1])
+    else
+        (xv[i-1], xv[i], xv[i]), (topo_y[i-1], topo_y[i], topo_y[i])
     end
+
+    ox   = xv[i]
+    oy   = yv[j]
+    for (l, ii) in enumerate((i - 1):i)
+        c += 1
+        !(0 < ii < nx) && continue
+        
+        ω += 1
+
+        ## new origin at the center of the (ii, jj)-th cell
+        origin = (ox, oy) .+ (mask_x[c], zero(T))
+        ## now we need to interpolate the segment of the chain to the boundaries of the new cell
+        # segment of the chain
+        p1 = GridGeometryUtils.Point(x[l], y[l])
+        p2 = GridGeometryUtils.Point(x[l+1], y[l+1])
+        # create a line from the two points
+        l  = Line(p1, p2)
+        # evaluate the line at the origin and origin + dx / 2
+        y1 = line(l, origin[1])
+        y2 = line(l, origin[1] + half_dx)
+        # create two points at the boundaries of the new cell
+        p1 = GridGeometryUtils.Point(origin[1], y1)
+        p2 = GridGeometryUtils.Point(origin[1] + half_dx, y2)
+        # and turn them into a segment
+        s  = Segment(p1, p2)
+
+        ## create a rectangle for the new cell
+        r    = Rectangle(origin, half_dx, half_dy)
+        tmp += cell_rock_area(s, r)
+    end
+    ratios[i,j] = tmp / ω
     return nothing
 end
 
 function compute_area_below_chain_vy!(ratio_velocity, chain, xvi, dxi)
-    topo_x, topo_y = chain.cell_vertices, chain.h_vertices
+    topo_y = chain.h_vertices
     nx, ny = size(ratio_velocity)
-    masks_x = ((0.0, 1.0), (0.0, 1.0))
-    masks_y = ((-0.5, 0.0), (0.0, 0.5))
+    mask_y = (-0.5, 0e0) .* dxi[2]
+
     @parallel (1:nx, 1:ny) _compute_area_below_chain_vy!(
-        ratio_velocity, topo_x, topo_y, masks_x, masks_y, xvi..., ny, dxi
+        ratio_velocity, topo_y, mask_y, xvi..., ny, dxi
     )
     return nothing
 end
 
 @parallel_indices (i, j) function _compute_area_below_chain_vy!(
-        ratios, topo_x, topo_y, masks_x, masks_y, xv, yv, n, dxi
-    )
+        ratios::AbstractArray{T}, topo_y, mask_y, xv, yv, ny, dxi
+    ) where {T}
     dx, dy = dxi
-    area_cell = prod(dxi) / 2
-    ratios[i, j] = zero(eltype(dx))
-    correction = 1
-    for (c, jj) in enumerate((j - 1):j)
-        if 0 < jj < n
-            if 1 < j < n
-                correction = 0.5
-            else
-                correction = 2.0e0
-            end
-            x_min_cell, x_max_cell = xv[i] .+ masks_x[c] .* dx
-            y_min_cell, y_max_cell = yv[jj] .+ masks_y[c] .* dy
+    half_dx = dx / 2
+    half_dy = dy / 2
+    c = 0
+    ω = 0 # weight for the average
+    tmp = zero(T)
+    # we can cache the potential coordinates
+    x, y = (xv[i], xv[i+1]), (topo_y[i], topo_y[i+1])
+    ox   = xv[i]
+    oy   = yv[j]
 
-            topo_xᵢ = topo_x[i], topo_x[i + 1]
-            topo_yᵢ = topo_y[i], topo_y[i + 1]
-            p1 = topo_xᵢ[1], topo_yᵢ[1]
-            p2 = topo_xᵢ[2], topo_yᵢ[2]
+    for (k, jj) in enumerate((j - 1):j)
+        c += 1
+        !(0 < jj < ny) && continue
+        
+        ω += 1
 
-            # recompute topography intersections
-            if do_intersect(p1, p2, x_min_cell, x_max_cell, y_min_cell, y_max_cell)
-                p1, p2 = get_intersections(
-                    (p1, p2), x_min_cell, x_max_cell, y_min_cell, y_max_cell
-                )
-                topo_xᵢ = p1[1], p1[2]
-                topo_xᵢ = p2[1], p2[2]
-            end
+        ## new origin at the center of the (ii, jj)-th cell
+        origin = (ox, oy) .+ (zero(T), mask_y[c])
+        ## now we need to interpolate the segment of the chain to the boundaries of the new cell
+        # segment of the chain
+        p1 = GridGeometryUtils.Point(x[1], y[1])
+        p2 = GridGeometryUtils.Point(x[2], y[2])
+        # create a line from the two points
+        l  = Line(p1, p2)
+        # evaluate the line at the origin and origin + dx / 2
+        y1 = line(l, origin[1])
+        y2 = line(l, origin[1] + half_dx)
+        # create two points at the boundaries of the new cell
+        p1 = GridGeometryUtils.Point(origin[1], y1)
+        p2 = GridGeometryUtils.Point(origin[1] + half_dx, y2)
+        # and turn them into a segment
+        s  = Segment(p1, p2)
 
-            isbelow = topo_yᵢ[1] - y_min_cell ≥ dy / 2 && topo_yᵢ[2] - y_min_cell ≥ dy / 2
-            isabove = y_max_cell - topo_yᵢ[1] ≥ dy / 2 && y_max_cell - topo_yᵢ[2] ≥ dy / 2
-
-            if isbelow
-                ratios[i, j] = min(1, ratios[i, j] + 1)
-
-            elseif isabove
-                ratios[i, j] = zero(eltype(topo_xᵢ))
-
-            else
-                # compute area at cell center
-                area_rock = compute_area_below_chain(
-                    topo_xᵢ, topo_yᵢ, x_min_cell, x_max_cell, y_min_cell, y_max_cell, dxi
-                )
-                ratios[i, j] += clamp(correction * area_rock / area_cell, 0, 1)
-            end
-        end
+        ## create a rectangle for the new cell
+        r    = Rectangle(origin, half_dx, half_dy)
+        tmp += cell_rock_area(s, r)
     end
+    ratios[i,j] = tmp / ω
+
     return nothing
 end
 
 function compute_area_below_chain_vertex!(ratio_vertex, chain, xvi, dxi)
-    topo_x, topo_y = chain.cell_vertices, chain.h_vertices
-    ni = size(ratio_vertex)
-    masks_x = ((-0.5, 0.0), (0.0, 0.5), (-0.5, 0.0), (0.0, 0.5))
-    masks_y = ((-0.5, 0.0), (-0.5, 0.0), (0.0, 0.5), (0.0, 0.5))
+    topo_y  = chain.h_vertices
+    ni      = size(ratio_vertex)
+    masks_x = (-0.5, 0e0, -0.5, 0e0) .* dxi[1]
+    masks_y = (-0.5, -0.5, 0e0, 0e0) .* dxi[2]
+
     @parallel (@idx ni) _compute_area_below_chain_vertex!(
-        ratio_vertex, topo_x, topo_y, masks_x, masks_y, xvi..., ni..., dxi
+        ratio_vertex, topo_y, masks_x, masks_y, xvi..., ni..., dxi
     )
     return nothing
 end
 
 @parallel_indices (i, j) function _compute_area_below_chain_vertex!(
-        ratios, topo_x, topo_y, masks_x, masks_y, xv, yv, nx, ny, dxi
-    )
+        ratios::AbstractArray{T}, topo_y, masks_x, masks_y, xv, yv, nx, ny, dxi
+    ) where {T}
     dx, dy = dxi
-    area_cell = prod(dxi) / 2
-    ratios[i, j] = zero(eltype(dx))
-    correction = 1
-    c = 0
-    for jj in (j - 1):j, ii in (i - 1):i
-        c += 1
-        if 0 < ii < nx && 0 < jj < ny
-            if 1 < i < nx && 1 < j < ny
-                correction = 0.5
-            else
-                correction = 2.0e0
-            end
-            x_min_cell, x_max_cell = xv[i] .+ masks_x[c] .* dx
-            y_min_cell, y_max_cell = yv[jj] .+ masks_y[c] .* dy
+    half_dx = dx / 2
+    half_dy = dy / 2
+    c = 0 # linear index of the mask
+    ω = 0 # weight for the average
+    tmp = zero(T)
+    # we can cache the potential coordinates
+    x, y =  if 1 < i < nx
+        (xv[i-1], xv[i], xv[i+1]), (topo_y[i-1], topo_y[i], topo_y[i+1])
+    elseif i == 1
+        (xv[i], xv[i], xv[i+1]), (topo_y[i], topo_y[i], topo_y[i+1])
+    else
+        (xv[i-1], xv[i], xv[i]), (topo_y[i-1], topo_y[i], topo_y[i])
+    end
 
-            topo_xᵢ = topo_x[ii], topo_x[ii + 1]
-            topo_yᵢ = topo_y[ii], topo_y[ii + 1]
-            p1 = topo_xᵢ[1], topo_yᵢ[1]
-            p2 = topo_xᵢ[2], topo_yᵢ[2]
+    ox = xv[i]
+    oy = yv[j]
 
-            # recompute topography intersections
-            if do_intersect(p1, p2, x_min_cell, x_max_cell, y_min_cell, y_max_cell)
-                p1, p2 = get_intersections(
-                    (p1, p2), x_min_cell, x_max_cell, y_min_cell, y_max_cell
-                )
-                topo_xᵢ = p1[1], p1[2]
-                topo_yᵢ = p2[1], p2[2]
-            end
+    for (k, jj) in enumerate((j - 1):j)
+        for (l, ii) in enumerate((i - 1):i)
+            c += 1
+            !(0 < jj < nx) && continue
+            !(0 < ii < ny) && continue
+            
+            ω += 1
 
-            # isbelow = topo_yᵢ[1] - y_min_cell ≥ dy / 2 && topo_yᵢ[2] - y_min_cell ≥ dy / 2
-            # isabove = y_max_cell - topo_yᵢ[1] ≥ dy / 2 && y_max_cell - topo_yᵢ[2] ≥ dy / 2
+            ## new origin at the center of the (ii, jj)-th cell
+            origin = (ox, oy) .+ (masks_x[c], masks_y[c])
+            ## now we need to interpolate the segment of the chain to the boundaries of the new cell
+            # segment of the chain
+            p1 = GridGeometryUtils.Point(x[l], y[k])
+            p2 = GridGeometryUtils.Point(x[l+1], y[k+1])
+            # create a line from the two points
+            l  = Line(p1, p2)
+            # evaluate the line at the origin and origin + dx / 2
+            y1 = line(l, origin[1])
+            y2 = line(l, origin[1] + half_dx)
+            # create two points at the boundaries of the new cell
+            p1 = GridGeometryUtils.Point(origin[1], y1)
+            p2 = GridGeometryUtils.Point(origin[1] + half_dx, y2)
+            # and turn them into a segment
+            s  = Segment(p1, p2)
 
-            # if isbelow
-            #     ratios[i, j] = min(1, ratios[i, j] + 1)
-
-            # # elseif isabove
-            # #     ratios[i, j] = zero(eltype(topo_xᵢ))
-
-            # else
-            # compute area at cell center
-            area_rock = compute_area_below_chain(
-                topo_xᵢ, topo_yᵢ, x_min_cell, x_max_cell, y_min_cell, y_max_cell, dxi
-            )
-            ratios[i, j] += clamp(correction * area_rock / area_cell, 0, 1)
-            # end
-            ratios[i, j] = clamp(ratios[i, j], 0, 1)
+            ## create a rectangle for the new cell
+            r    = Rectangle(origin, half_dx, half_dy)
+            tmp += cell_rock_area(s, r)
         end
     end
+    ratios[i,j] = tmp / ω
+    
     return nothing
 end
 
 #############################
 
-function compute_area_below_chain(
-        topo_xᵢ, topo_yᵢ, x_min_cell::T, x_max_cell::T, y_min_cell::T, y_max_cell::T, dxi
-    ) where {T <: Real}
-    dx, dy = dxi
-    area = zero(T)
+@inline function is_chain_above_cell(s::Segment, r::Rectangle)
+    max_y = r.origin[2] + r.h
+    # Check if the segment is above the rectangle
+    return s.p1[2] ≥ max_y && s.p2[2] ≥ max_y
+end
 
-    is_chain_within_cell = true
-    for y in topo_yᵢ
-        is_chain_within_cell *= y_min_cell ≤ y ≤ y_max_cell
-    end
+@inline function is_chain_below_cell(s::Segment, r::Rectangle)
+    min_y = r.origin[2]
+    # Check if the segment is below the rectangle
+    return s.p1[2] ≤ min_y && s.p2[2] ≤ min_y
+end
 
-    # whole topograpy segment is within the cell
-    # := trapezoid area
-    if is_chain_within_cell
-        area += trapezoid_area((topo_yᵢ .- y_min_cell)..., dx)
-
+function cell_rock_area(s::Segment, r::Rectangle{T}) where {T}
+    A = if is_chain_above_cell(s, r)
+        one(T)
+    elseif is_chain_below_cell(s, r)
+        zero(T)
     else
-        # coordinates of topography segment
-        p1 = topo_xᵢ[1], topo_yᵢ[1]
-        p2 = topo_xᵢ[2], topo_yᵢ[2]
-        p = p1, p2
-
-        # compute intersections between topography segment and cell boundaries
-        intersections = get_intersections(p, x_min_cell, x_max_cell, y_min_cell, y_max_cell)
-
-        # compute inner area
-        dx_intersections = intersections[2][1] - intersections[1][1]
-        area += trapezoid_area(
-            intersections[1][2] - y_min_cell,
-            intersections[2][2] - y_min_cell,
-            dx_intersections,
-        )
-
-        # compute area of the tails, if necessary
-        # compute left-hand-side tail
-        if intersections[1][2] ≥ y_max_cell
-            lx = intersections[1][1] - x_min_cell
-            area += rectangle_area(lx, dy)
-        end
-        # compute right-hand-side tail
-        if intersections[2][2] ≥ y_max_cell
-            lx = x_max_cell - intersections[1][1]
-            area += rectangle_area(lx, dy)
-        end
+        clamp(intersecting_area(s, r) / area(r), zero(T), one(T))
     end
-
-    return area
-end
-
-### Utils
-
-@inline trapezoid_area(a, b, h) = (a + b) * h / 2
-@inline rectangle_area(lx, ly) = lx * ly
-
-function slope_intercept(p1, p2)
-    # unpack
-    x1, y1 = p1
-    x2, y2 = p2
-    # compute slope and intercept
-    slope = (y2 - y1) / (x2 - x1)
-    intercept = y1 - slope * x1
-
-    return slope, intercept
-end
-
-function line_intersection(p1, p2, q1, q2)
-    # compute slopes and intercepts
-    a, c = slope_intercept(p1, p2)
-    b, d = slope_intercept(q1, q2)
-    # compute intersection point
-    px = (isinf(a) || a == b) ? p1[1] : (d - c) / (a - b)
-    py = @muladd b * px + d
-    return (px, py)
-end
-
-function get_intersections(p::NTuple{2}, x_min_cell, x_max_cell, y_min_cell, y_max_cell)
-    p1, p2 = p
-    intersections = Base.@ntuple 2 ii -> begin
-        @inline
-        pᵢ = p[ii]
-        intersection = if pᵢ[2] > y_max_cell
-            line_intersection(p1, p2, (x_min_cell, y_max_cell), (x_max_cell, y_max_cell))
-        elseif pᵢ[2] < y_min_cell
-            line_intersection(p1, p2, (x_min_cell, y_min_cell), (x_max_cell, y_min_cell))
-        else
-            pᵢ
-        end
-    end
-    return intersections
-end
-
-function find_minmax_cell_indices(topo_yᵢ, origin_y, dy)
-    ymin, ymax = extrema(topo_yᵢ)
-    min_cell_j = Int((ymin - origin_y) ÷ dy) + 1
-    max_cell_j = Int((ymax - origin_y) ÷ dy) + 1
-    return min_cell_j, max_cell_j
-end
-
-# check if the topography segment intersects with the lateral cell boundaries;
-# if it does, it does intersect with the cell
-function do_intersect(p1, p2, x_min_cell, x_max_cell, y_min_cell, y_max_cell)
-    _, left_y = line_intersection(
-        p1, p2, (x_min_cell, y_min_cell), (x_min_cell, y_max_cell)
-    )
-    y_min_cell ≤ left_y ≤ y_max_cell && return true
-    _, right_y = line_intersection(
-        p1, p2, (x_max_cell, y_min_cell), (x_max_cell, y_max_cell)
-    )
-    y_min_cell ≤ right_y ≤ y_max_cell && return true
-
-    return false
+    return A 
 end
