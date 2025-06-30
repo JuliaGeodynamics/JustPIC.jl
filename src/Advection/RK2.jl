ebfafb37bc0310d01ea5211e56b53c93c5f8cf38
Albert de Montserrat <albert.de.montserrat@gmail.com>
2025-06-30 16:06:56 +0200

    add sign of time integration to allow for backtracking

--- src/Advection/RK2.jl
+++ src/Advection/RK2.jl
@@ -2,10 +2,13 @@
         integrator::RungeKutta2,
         dt,
         particle_velocity::NTuple{N, T},
-        particle_coordinate::NTuple{N, T},
+        particle_coordinate::NTuple{N, T};
+        backtracking::Bool = false
     ) where {N, T}
     (; α) = integrator
-    return @. @muladd particle_coordinate + dt * α * particle_velocity
+    backtracking_sign = 1 - 2 * backtracking # flip sign if backtracking is true, used for backtracking particles during Semi-Lagrangian advection
+    @show backtracking_sign
+    return @. @muladd particle_coordinate +  dt * particle_velocity
 end
 
 @inline function second_stage(
@@ -13,14 +16,16 @@ end
         dt,
         particle_velocity0::NTuple{N, T},
         particle_velocity1::NTuple{N, T},
-        particle_coordinate::NTuple{N, T},
+        particle_coordinate::NTuple{N, T};
+        backtracking::Bool = false
     ) where {N, T}
     (; α) = integrator
+    backtracking_sign = 1 - 2 * backtracking # flip sign if backtracking is true, used for backtracking particles during Semi-Lagrangian advection
     p = if α == 0.5
-        @. @muladd particle_coordinate + dt * particle_velocity1
+        @. @muladd particle_coordinate + backtracking_sign * dt * particle_velocity1
     else
         @. @muladd particle_coordinate +
-            dt * (
+            backtracking_sign * dt * (
             (1.0 - 0.5 * inv(α)) * particle_velocity0 + 0.5 * inv(α) * particle_velocity1
         )
     end
--- src/Particles/Advection/RK2.jl
+++ src/Particles/Advection/RK2.jl
@@ -6,23 +6,25 @@
         local_limits,
         dxi,
         dt,
-        idx::NTuple{N},
+        idx::NTuple{N};
+        backtracking::Bool = false
     ) where {N}
 
     # interpolate velocity to current location
     vp0 = interp_velocity2particle(p0, grid_vi, local_limits, dxi, V, idx)
 
     # first advection stage x = x + v * dt * α
-    p1 = first_stage(method, dt, vp0, p0)
+    p1 = first_stage(method, dt, vp0, p0; backtracking = backtracking)
 
     # interpolate velocity to new location
     vp1 = interp_velocity2particle(p1, grid_vi, local_limits, dxi, V, idx)
 
     # final advection step
-    p2 = second_stage(method, dt, vp0, vp1, p0)
+    p2 = second_stage(method, dt, vp0, vp1, p0; backtracking = backtracking)
 
     return p2
 end
+
 @inline function advect_particle(
         method::RungeKutta2,
         p0::NTuple{N},
@@ -32,20 +34,21 @@ end
         dxi,
         dt,
         interpolation_fn::F,
-        idx::NTuple,
+        idx::NTuple;
+        backtracking::Bool = false
     ) where {N, F}
 
     # interpolate velocity to current location
     vp0 = interpolation_fn(p0, grid_vi, local_limits, dxi, V, idx)
 
     # first advection stage x = x + v * dt * α
-    p1 = first_stage(method, dt, vp0, p0)
+    p1 = first_stage(method, dt, vp0, p0; backtracking = backtracking)
 
     # interpolate velocity to new location
     vp1 = interpolation_fn(p1, grid_vi, local_limits, dxi, V, idx)
 
     # final advection step
-    p2 = second_stage(method, dt, vp0, vp1, p0)
+    p2 = second_stage(method, dt, vp0, vp1, p0; backtracking = backtracking)
 
     return p2
 end
