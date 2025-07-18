c782c3026726985a2ded4623451ca8db741d91d6
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-07-18 11:09:48 +0200

    RK4 compatibility

--- src/Particles/Advection/RK4.jl
+++ src/Particles/Advection/RK4.jl
@@ -39,3 +39,48 @@ end
 
     return p
 end
+
+
+@inline function advect_particle_SML(
+        ::RungeKutta4,
+        p0::NTuple{N},
+        V::NTuple{N},
+        grid_vi,
+        dxi,
+        dt,
+        idx::NTuple{N};
+        backtracking::Bool = false
+    ) where {N}
+    
+    backtracking_sign = 1 - 2 * backtracking # flip sign if backtracking is true, used for backtracking particles during Semi-Lagrangian advection
+    k1 = interp_velocity2particle(p0, grid_vi, dxi, V, idx)
+    k2 = interp_velocity2particle(p0 .+ dt .* k1 ./ 2, grid_vi, dxi, V, idx)
+    k3 = interp_velocity2particle(p0 .+ dt .* k2 ./ 2, grid_vi, dxi, V, idx)
+    k4 = interp_velocity2particle(p0 .+ dt .* k3, grid_vi, dxi, V, idx)
+
+    p = @. p0 + dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+    return p
+end
+
+@inline function advect_particle_SML(
+        ::RungeKutta4,
+        p0::NTuple{N},
+        V::NTuple{N},
+        grid_vi,
+        dxi,
+        dt,
+        interpolation_fn::F,
+        idx::NTuple;
+        backtracking::Bool = false
+    ) where {N, F}
+    
+    backtracking_sign = 1 - 2 * backtracking # flip sign if backtracking is true, used for backtracking particles during Semi-Lagrangian advection
+    k1 = interpolation_fn(p0, grid_vi, dxi, V, idx)
+    k2 = interpolation_fn(p0 .+ backtracking_sign .* dt .* k1 ./ 2, grid_vi, dxi, V, idx)
+    k3 = interpolation_fn(p0 .+ backtracking_sign .* dt .* k2 ./ 2, grid_vi, dxi, V, idx)
+    k4 = interpolation_fn(p0 .+ backtracking_sign .* dt .* k3, grid_vi, dxi, V, idx)
+
+    p = @. p0 + backtracking_sign * dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+    return p
+end
