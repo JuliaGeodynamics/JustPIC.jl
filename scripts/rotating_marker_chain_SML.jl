22c9fb6f26b2aae151233b058912dca4597ddbb7
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-07-18 16:47:30 +0200

    add example of MC+SML

---
+++ scripts/rotating_marker_chain_SML.jl
@@ -0,0 +1,74 @@
+using JustPIC
+using JustPIC._2D
+using ParallelStencil
+@init_parallel_stencil(Threads, Float64, 2)
+using GLMakie
+
+const backend = JustPIC.CPUBackend
+
+function expand_range(x::AbstractRange)
+    dx = x[2] - x[1]
+    n = length(x)
+    x1, x2 = extrema(x)
+    xI = x1 - dx
+    xF = x2 + dx
+    return LinRange(xI, xF, n + 2)
+end
+
+# Analytical flow solution
+vi_stream(x) = π * 1.0e-5 * (x - 0.5)
+
+function main()
+
+    # Initialize particles -------------------------------
+    n = 51
+    nx = n - 1
+    Lx = Ly = 1.0
+    # nodal vertices
+    xvi = xv, yv = LinRange(0, Lx, n), LinRange(0, Ly, n)
+    dxi = dx, dy = xv[2] - xv[1], yv[2] - yv[1]
+    # nodal centers
+    xc, yc = LinRange(0 + dx / 2, Lx - dx / 2, n - 1), LinRange(0 + dy / 2, Ly - dy / 2, n - 1)
+    # staggered grid velocity nodal locations
+    grid_vx = xv, expand_range(yc)
+    grid_vy = expand_range(xc), yv
+    grid_vxi = grid_vx, grid_vy
+
+    # Cell fields -------------------------------
+    Vx = TA(backend)([-vi_stream(y) for x in grid_vx[1], y in grid_vx[2]])
+    Vy = TA(backend)([ vi_stream(x) for x in grid_vy[1], y in grid_vy[2]])
+
+    xc0 = yc0 = 0.25
+    R = 24 * dx
+    T = TA(backend)([ ((x - xc0)^2 + (y - yc0)^2 ≤ R^2) * 1.0 for x in xv, y in yv])
+    V = Vx, Vy
+
+    w = π * 1.0e-5  # angular velocity
+    period = 1  # revolution number
+    tmax = period / (w / (2 * π))
+    dt = 200.0
+
+    nxcell, min_xcell, max_xcell = 12, 6, 24
+    initial_elevation = Ly / 2
+    chain = init_markerchain(backend, nxcell, min_xcell, max_xcell, xv, initial_elevation)
+    chain.h_vertices .= initial_elevation 
+    method = RungeKutta4()
+
+    for _ in 1:125
+        semilagrangian_advection_markerchain!(chain, method, V, grid_vxi, xvi, dt)
+    end
+
+    f = Figure(size = (1200,1200))
+    ax = Axis(f[1, 1])
+    # vector of shapes
+    poly!(
+        ax,
+        Rect(0, 0, 1, 1),
+        color = :lightgray,
+    )
+    lines!(ax, xvi[1], chain.h_vertices, color = :blue, linewidth = 4)
+    display(f)
+    return nothing
+end
+
+main()
