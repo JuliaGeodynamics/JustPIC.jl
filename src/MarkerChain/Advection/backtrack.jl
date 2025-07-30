c64124f325eecd6af1cdb4a3279c88270c395559
Pascal Aellig <pascal.aellig@hotmail.com>
2025-07-30 10:51:54 +0200

    rm obsolte copy call

--- src/MarkerChain/Advection/backtrack.jl
+++ src/MarkerChain/Advection/backtrack.jl
@@ -3,7 +3,7 @@ using Statistics
 function semilagrangian_advection_markerchain!(
         chain::MarkerChain, method::AbstractAdvectionIntegrator, V, grid_vxi, grid, dt
     )
-    copyto!(chain.h_vertices0, chain.h_vertices)
+
     semilagrangian_advection!(chain, method, V, grid_vxi, grid, dt)
     # correct topo to conserve mass
     chain.h_vertices .+= mean(chain.h_vertices) - mean(chain.h_vertices0)
